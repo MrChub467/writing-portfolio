a97a86596af52b2ce9233829808a43348d57b924
MrChub467 <chubster467@gmail.com>
2025-12-20 15:48:23 -0600

    add js for testing purposes

---
+++ app.js
@@ -0,0 +1,7 @@
+document.querySelector(".book").addEventListener("click", (e) => {
+  console.log(e.target);
+});
+
+// document.querySelector(".book").addEventListener("mouseover", (e) => {
+//   console.log(e.target);
+// });
