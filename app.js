ecda7c82a4b3f1ba72d383193e2ea40797fc5fe8
MrChub467 <chubster467@gmail.com>
2025-12-22 17:49:16 -0600

    add page flips on table of contents

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-document.querySelector(".book").addEventListener("click", (e) => {
-  console.log(e.target);
-});
+const one = document.querySelector(".chapter-one");
+const two = document.querySelector(".chapter-two");
+const three = document.querySelector(".chapter-three");
+const four = document.querySelector(".chapter-four");
 
-// document.querySelector(".book").addEventListener("mouseover", (e) => {
-//   console.log(e.target);
-// });
+const pages = Array.from(document.querySelectorAll("input"));
+
+one.addEventListener("click", (e) => {
+  for (let i = 0; i < 2; ++i) {
+    setTimeout(() => {
+      pages[i].checked = "true";
+    }, 200 * i);
+  }
+});
+two.addEventListener("click", (e) => {
+  for (let i = 0; i < 3; ++i) {
+    setTimeout(() => {
+      pages[i].checked = "true";
+    }, 200 * i);
+  }
+});
+three.addEventListener("click", (e) => {
+  for (let i = 0; i < 5; ++i) {
+    setTimeout(() => {
+      pages[i].checked = "true";
+    }, 200 * i);
+  }
+});
+four.addEventListener("click", (e) => {
+  for (let i = 0; i < 6; ++i) {
+    setTimeout(() => {
+      pages[i].checked = "true";
+    }, 40 * i);
+  }
+});
